759e45e41bc49fcb26718828f572d5c4a504bccc
BasiraCollection <ismailalimiddya98765@gmail.com>
2026-03-16 01:53:20 +0530

    Update product data fetching and addToCart function

--- Store.js
+++ Store.js
@@ -1,5 +1,5 @@
-fetch("data/products.json")
-.then(res => res.json())
+fetch("Product.json")
+.then(response => response.json())
 .then(data => {
 
 let container = document.getElementById("product-list");
@@ -7,13 +7,14 @@ let container = document.getElementById("product-list");
 data.products.forEach(product => {
 
 let div = document.createElement("div");
+
 div.className = "product";
 
 div.innerHTML = `
-<img src="${product.images[0]}">
+<img src="${product.images[0]}" alt="">
 <h3>${product.name}</h3>
 <p>₹${product.price}</p>
-<button onclick="addToCart(${product.id})">Add to Cart</button>
+<button onclick="addToCart('${product.name}')">Add to Cart</button>
 `;
 
 container.appendChild(div);
@@ -21,3 +22,9 @@ container.appendChild(div);
 });
 
 });
+
+function addToCart(name){
+
+alert(name + " added to cart");
+
+}
